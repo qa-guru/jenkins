eac5678f80856980bd747bc467695ce272bf2103
svasenkov <aanher@gmail.com>
2021-07-29 20:38:14 +0300

    added more examples with SystemProperties

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -9,4 +9,34 @@ public class SystemPropertiesTests {
     void test1() {
         System.out.println("[test1] Browser: " + System.getProperty("browser"));
     }
+
+    @Test
+    void readNullFromPropertyTest() {
+        String value = System.getProperty("our_property");
+        System.out.println(value);
+        // null
+    }
+
+    @Test
+    void readDefaultFromPropertyTest() {
+        String value = System.getProperty("our_property", "default_value");
+        System.out.println(value);
+        // default_value
+    }
+
+    @Test
+    void readWithTerminalFromPropertyTest() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+
+        //  gradle clean properties_tests
+        //  gradle clean properties_tests -Dbrowser=safari
+        //  gradle clean properties_tests -Dbrowser=safari -Dversion=89
+    }
 }
